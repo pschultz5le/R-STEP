3edade04e6a535fbbb35a48c6447b21d34224927
pschultz5le <pschultz@5lakesenergy.com>
2025-10-24 11:21:40 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,7 +60,8 @@ def render_field(row, key_prefix: str, current_value):
     if t in ("number", "percentage"):
         # use float everywhere to avoid StreamlitMixedNumericTypesError
         if t == "percentage":
-            step = 0.1
+            step = 0.1,
+            t.map(format_number)
         else:
             step = 1
         val = _to_float(current_value)
